7493eff5e9f110549a285440432b6eaacd43a8f3
Rohan Prajapati <190770107222@socet.edu.in>
2023-04-20 18:51:18 +0530

    feat(arithmetic):arithmetic-operation-js-task

--- arithmetic-operations.js
+++ arithmetic-operations.js
@@ -4,25 +4,16 @@ const b = 37;
 console.log("result", a + b);
 
 //substraction of two Number
-const p = 25;
-const q = 37;
 console.log("result", p - q);
 
 //Multiplication of two Number
-const x = 10;
-const y = 5;
 console.log("result", x * y);
 
 //Division of two Number
-const d = 10;
-const e = 5;
 console.log("result", d / e);
 
 //Square of two Number
-let s = 25;
 console.log( "result",s ** 2);
 
 //concatenate two string
-let text = "This is a very";
-text += "beautiful scene";
 console.log("result",text);
